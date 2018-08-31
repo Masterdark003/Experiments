a85ca1ab3a04e6e23ad5a967348d05dfa3e15beb
Masterdark003 <Masterdark003@test.github.com>
2018-08-31 14:30:56 -0500

    1 -10

--- Exp/src/exp/Exp.java
+++ Exp/src/exp/Exp.java
@@ -16,6 +16,9 @@ public class Exp {
      */
     public static void main(String[] args) {
         System.out.println("Holi");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
     
 }
